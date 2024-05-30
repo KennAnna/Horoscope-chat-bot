009ef719c864ee2a457a2951782a2b59406bf5a7
KennAnna <160725569+KennAnna@users.noreply.github.com>
2024-05-30 22:48:30 +0300

    Created chatBot with horoscope

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+const TelegramBot = require('node-telegram-bot-api');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+private ListSign: string[]=[
+   'aries',
+    'taurus',
+    'gemini',
+    'cancer',
+    'leo',
+    'virgo',
+    'libra',
+    'scorpio',
+    'sagittarius',
+    'capricorn',
+    'aquarius',
+    'pisces'
+  ]
+  @Cron('10 * * * * *')
+  async getHello() {
+    const token = '';
+    const bot = new TelegramBot(token, { polling: false });
+    for (const sign of this.ListSign) {
+      const message = await this.getTodayHoroscope(sign);
+      //0 = chatId
+      bot.sendMessage(0, message);
+    }
+
+
+  }
+
+  async getTodayHoroscope(sign: string): Promise<string> {
+    const url = `https://www.marieclaire.ru/astro/${sign}/day/`;
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    return $('.block-text').text();
   }
 }
